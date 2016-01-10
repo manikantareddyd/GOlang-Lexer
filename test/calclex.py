310f3074cb09feb151538004ed77848efb86713e
Manikanta Reddy D <manikant@cse.iitk.ac.in>
2016-01-10 11:45:31 +0530

    Reorganisation of Directory structure for easy organization

--- test/calclex.py
+++ test/calclex.py
@@ -1,49 +1,42 @@
-# -----------------------------------------------------------------------------
-# calclex.py
-# -----------------------------------------------------------------------------
-import sys
-
-if ".." not in sys.path: sys.path.insert(0,"..")
 import ply.lex as lex
 
+# List of token names.   This is always required
 tokens = (
-    'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
-    )
+   'NUMBER',
+   'PLUS',
+   'MINUS',
+   'TIMES',
+   'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+)
 
-# Tokens
-
+# Regular expression rules for simple tokens
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
-t_EQUALS  = r'='
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
+# A regular expression rule with some action code
 def t_NUMBER(t):
     r'\d+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %s" % t.value)
-        t.value = 0
+    t.value = int(t.value)    
     return t
 
-t_ignore = " \t"
-
+# Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-    
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-    
+
 # Build the lexer
 lexer = lex.lex()
-
-
-
