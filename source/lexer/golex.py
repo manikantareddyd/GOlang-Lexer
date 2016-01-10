deb681ef3f5dd39171fd864024c0969ab1f82e44
Manikanta Reddy D <manikant@cse.iitk.ac.in>
2016-01-10 13:44:33 +0530

    The following have currently been handled
    1. Operators
    2. Identifiers
    3. Reserved Keywords
    4. Comments // and /* */
    5. Newline
    6. Spaces

--- source/lexer/golex.py
+++ source/lexer/golex.py
@@ -16,6 +16,8 @@ def t_error(t):
 
 t_ignore_COMMENT = r'(?://[^\n]*|/\*(?:(?!\*/).)*\*/)'
 
+
+#These are a set of reserved tokens in GO, NOT to be used as keywords
 reserved = {
         'break':'BREAK',
         'default':'DEFAULT',
@@ -43,6 +45,7 @@ reserved = {
         'return':'RETURN',
         'var':'VAR'
         }
+
 # List of token names.   This is always required
 tokens = [
         'NUMBER',
@@ -50,9 +53,50 @@ tokens = [
         'MINUS',
         'TIMES',
         'DIVIDE',
+        'MOD',
+        'AMPERS',
+        'OR',
+        'CARET',
+        'LL',
+        'GG',
+        'AMPCAR',
+        'MINUSEQ',
+        'DIVIDEEQ',
+        'MODEQ',
+        'AMPEQ',
+        'OREQ',
+        'CAREQ',
+        'LLEQ',
+        'GGEQ',
+        'AMPCAREQ',
+        'AMPAMP',
+        'OROR',
+        'LMINUS',
+        'PLUSPLUS',
+        'MINUSMIN',
+        'EQEQ',
+        'LESS',
+        'GREAT',
+        'EQUAL',
+        'NOT',
+        'NOTEQ',
+        'LEQ',
+        'GEQ',
+        'COLONEQ',
+        'DDD',
         'LPAREN',
         'RPAREN',
-        'IDENTIFIER',   
+        'LBRACK',
+        'RBRACK',
+        'LBRACE',
+        'RBRACE',
+        'COMMA',
+        'DOT',
+        'SEMICOL',
+        'COLON',
+        'IDENTIFIER', 
+        'PLUSEQ',
+        'TIMESEQ'  
         ] + list(reserved.values())
 
 # Regular expression rules for simple tokens
@@ -60,8 +104,50 @@ t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
+t_MOD     = r'%'
+t_AMPERS  = r'&'
+t_OR 	  = r'\|'
+t_CARET   = r'\^'
+t_LL      = r'<<'
+t_GG	  = r'>>'
+t_AMPCAR  = r'&\^'
+t_PLUSEQ  = r'\+='
+t_MINUSEQ = r'-='
+t_TIMESEQ = r'\*='
+t_DIVIDEEQ= r'/='
+t_MODEQ   = r'%='
+t_AMPEQ   = r'&='
+t_OREQ    = r'\|='
+t_CAREQ   = r'\^='
+t_LLEQ    = r'<<='
+t_GGEQ    = r'>>='
+t_AMPCAREQ= r'&\^='
+t_AMPAMP  = r'&&'
+t_OROR    = r'\|\|'
+t_LMINUS  = r'<-'
+t_PLUSPLUS= r'\+\+'
+t_MINUSMIN= r'--'
+t_EQEQ    = r'=='
+t_LESS    = r'<'
+t_GREAT   = r'>'
+t_EQUAL   = r'='
+t_NOT     = r'!'
+t_NOTEQ   = r'!='
+t_LEQ     = r'<='
+t_GEQ     = r'>='
+t_COLONEQ = r':='
+t_DDD	  = r'\.\.\.'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
+t_LBRACK  = r'\['
+t_RBRACK  = r'\]'
+t_LBRACE  = r'\{'
+t_RBRACE  = r'\}'
+t_COMMA   = r'\,'
+t_DOT     = r'\.'
+t_SEMICOL = r'\;'
+t_COLON   = r'\:'  
+
 
 
 def t_IDENTIFIER(t):
