6ce126cc72861a240966227a2feded054337f0c5
Manikanta Reddy D <manikant@cse.iitk.ac.in>
2016-01-10 11:58:47 +0530

    Enlisted Keywords in GO

---
+++ source/lexer/golex.py
@@ -0,0 +1,32 @@
+import ply.lex as lex
+
+# List of Keywords to be present in GO, these are reserved and are not to be used as identifiers
+keywords = (
+	'break',
+	'default',
+	'func',
+	'interface',
+	'select',
+	'case',
+	'defer',
+	'go',
+	'map',
+	'struct',
+	'chan',
+	'else',
+	'goto',
+	'package',
+	'switch',
+	'const',
+	'fallthrough',
+	'if',
+	'range',
+	'type',
+	'continue',
+	'for',
+	'import',
+	'return',
+	'var'
+)
+
+
