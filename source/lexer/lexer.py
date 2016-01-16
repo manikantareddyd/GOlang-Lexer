f5fac7b25be7536a214c58b0b289eb1e360179c6
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-01-16 14:17:04 +0530

    Statistics are now printed

--- source/lexer/lexer.py
+++ source/lexer/lexer.py
@@ -2,6 +2,9 @@ import sys
 import ply.lex as lex
 from regexes import *
 
+Toks={}
+for a in tokens:
+    Toks[a]=[a,0]
 
 #Scanning the file name
 if (len(sys.argv) == 1):
@@ -16,8 +19,17 @@ try:
         data += '\n'
         lexer.input(data)
         print '\n'
-       	for tok in lexer:
-       		print tok.type, tok.value
-            
+        NoOfTok=0
+        for tok in lexer:
+            lis = Toks[tok.type]
+            lis[1]=lis[1]+1
+            lis.append(tok.value)
+            Toks[tok.type]=lis
+              
+       	for x in Toks:
+            lis=Toks[x]
+            if lis[1]!=0 :
+                print lis[0],lis[1],lis[2:]
+
 except IOError as e:
     print "I/O error({0}): "+ "We are not able to open " + file_name + " . Does it Exists? Check permissionsi!"
