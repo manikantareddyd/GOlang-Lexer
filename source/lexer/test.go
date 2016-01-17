e09475fcc125643d7a9eb2dd2c763e62201dcd18
Parag Bansal <paragb@cse.iitk.ac.in>
2016-01-17 18:59:09 +0530

    nothing

--- source/lexer/test.go
+++ source/lexer/test.go
@@ -1,3 +1,7 @@
+#Things to be done
+    1) add a token naming illegal character!
+    2) Proper formatting of output.
+    3)
 222
 Hi+90
 ---
@@ -12,3 +16,14 @@ x=-30
 8+i8
 int 20
 -90.7
+return
+var a=100
+if temp==2:
+    print "What the fuck?\n"
+    else:
+    print "no fucking ida?"
+    switch jasld;fj.asjdf.lasjdf.:
+    print test
+    but id dont
+    asdlfsdfk;lajdf
+45646.45646.564.4654
